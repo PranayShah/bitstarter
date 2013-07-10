bd095eae631b0970d2ce4e86e5a588ff2c2a48af
Pranay Shah <greatpranayshah@gmail.com>
2013-07-10 13:27:59 +0000

    Added index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var buf = new Buffer ();
+    buf - fs.readFileSync(index.html);
+    buf.toString();
+    response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
